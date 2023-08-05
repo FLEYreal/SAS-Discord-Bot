3125bec4e1d8721a5f14b0c45ca1216464cc94e7
FLEYreal <borisov.nikita.off@gmail.com>
2023-08-05 18:32:28 +0800

    Upload the bot

---
+++ config.py
@@ -0,0 +1,26 @@
+# Login for account that checks the XP
+# This account can't be used at the same time as the checker is running
+STEAM_USERNAME = "taekwondoler_1990"
+STEAM_PASSWORD = "Rwg8AbhO"
+
+# Steam API key for getting account name and avatar
+# If you disable it, you wont see user's names or avatars
+STEAM_API_KEY = "0E95D2D95964609F4D826E473D1257D8"
+DISABLE_STEAM_API = False
+
+# Path where logins for Steam will be saved
+CREDENTIALS_LOCATION = "credentials"
+
+# Webhook that will be used to post XP updates
+DISCORD_UPDATE_WEBHOOK = "https://discord.com/api/webhooks/1134378383397109810/h0m-ATXkVA4Az4sWKtu0qhN2nKOmtpaZM6FtxUERCAEqyNZ09RLq1abhv-8R6CMYsFNg"
+WEBHOOK_USERNAME = "XP Tracker"
+WEBHOOK_AVATAR_URL = "https://cdn.discordapp.com/embed/avatars/0.png"
+
+# Path for list of users being tracked
+TRACKING_LIST_PATH = "tracking_list.json"
+
+# Send message if user is added/removed
+SEND_TRACKING_LIST_UPDATES = True
+
+# Timeout between checks (in seconds)
+CHECK_TIMEOUT = 60
