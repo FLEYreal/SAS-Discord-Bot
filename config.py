285d0230d44c41bbebfa9b54bc182391075117e5
FLEY <129403933+FLEYreal@users.noreply.github.com>
2023-09-30 07:15:26 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# Login for account that checks the XP
+# This account can't be used at the same time as the checker is running
+STEAM_USERNAME = ""
+STEAM_PASSWORD = ""
+
+# Steam API key for getting account name and avatar
+# If you disable it, you wont see user's names or avatars
+STEAM_API_KEY = ""
+DISABLE_STEAM_API = False
+
+# Path where logins for Steam will be saved
+CREDENTIALS_LOCATION = "credentials"
+
+# Path for list of users being tracked
+TRACKING_LIST_PATH = "tracking_list.json"
+
+# Send message if user is added/removed
+SEND_TRACKING_LIST_UPDATES = True
+
+# Timeout between checks (in seconds)
+CHECK_TIMEOUT = 60
+
+# Discord Token
+TOKEN = ""
